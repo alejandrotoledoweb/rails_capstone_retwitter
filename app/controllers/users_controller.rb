16a3a162956eab90aa2aa00fad8c4a9b6cc20269
AlejandroToledo <atoledofr@gmail.com>
2021-01-07 15:26:08 -0500

    created the singup page and methods for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'Welcome to the Retwitter page'
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
+    @user = User.new
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:fullname, :username, :email, :password,
+                                 :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :fullname, presence: true, length: { in: 3..20 }
   validates :username, presence: true, uniqueness: true, length: { in: 3..20 }
-  VALID_EMAIL_REGEX.freeze = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
 
   has_secure_password
