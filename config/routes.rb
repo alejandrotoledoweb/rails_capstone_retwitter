046abf7f41b2f95cbb128acd4e670cff1c386c9a
AlejandroToledo <atoledofr@gmail.com>
2021-01-07 11:46:13 -0500

    create users controller and create the routes for signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'static_pages#home'
-  get  'static_pages/help'
-  get  'static_pages/about'
-  get  'static_pages/contact'
+  root             'static_pages#home'
+  get 'help'    => 'static_pages#help'
+  get 'about'   => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+  get 'signup'  => 'users#new'
 end
