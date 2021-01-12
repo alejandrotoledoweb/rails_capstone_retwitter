6c415a0d2b0003884e4fcad352798e6140806e5a
AlejandroToledo <atoledofr@gmail.com>
2021-01-12 11:54:54 -0500

    create test for user and relationship models

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    user = User.new
+    user.username = 'User'
+    user.email = 'user@email.com'
+    user.fullname = 'fullname1'
+    user.password = '111111'
+    user.password_confirmation = '111111'
+
+    user1 = User.new
+    user1.username = 'User1'
+    user1.email = 'user1@email.com'
+    user1.fullname = 'fullname2'
+    user1.password = '222222'
+    user1.password_confirmation = '222222'
+  end
+
+  it 'is test to create a following' do
+    user = User.new
+    user1 = User.new
+    follow = Relationship.new(follower_id: user.id, followed_id: user1.id)
+    expect(follow).to_not be_valid
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:follower_id) }
+    it { should validate_presence_of(:followed_id) }
+  end
+
+  describe 'associations' do
+    it { should belong_to(:follower).class_name('User') }
+    it { should belong_to(:followed).class_name('User') }
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe User, type: :model do
     user.username = 'User1'
     user.email = 'user@email.com'
     user.fullname = 'fullname1'
+    user.password = '111111'
+    user.password_confirmation = '111111'
     expect(user).to be_valid
   end
 
@@ -16,33 +18,26 @@ RSpec.describe User, type: :model do
   end
 
   it 'checks email uniquness' do
-    user2 = User.create(fullname: 'Juan andres', email: 'alex@email.com', username: 'secretpass')
-    user2.valid?
+    user2 = User.create(fullname: 'Juan andres', email: 'alex@email.com', username: 'secretpass',
+                        password: '111111', password_confirmation: '111111')
     expect(user2.valid?).to eql(true)
 
-    user2 = User.create(fullname: 'joha', email: 'joha@email.com', username: 'secretpass')
+    user2 = User.create(fullname: 'joha', email: 'alex@email.com', username: 'secretpass',
+                        password: '111111', password_confirmation: '111111')
     expect(user2.valid?).to eql(false)
   end
 
-  # describe 'associations' do
-  #   subject { User.create(name: 'alex', email: 'alex@gmail.com', password: '123456') }
-  #   it { should validate_presence_of(:name) }
-  #   it { should validate_presence_of(:email) }
-  #   it { should validate_presence_of(:password) }
-  # end
-  # describe 'associations' do
-  #   it { should have_many(:posts) }
-  #   it { should have_many(:comments) }
-  #   it { should have_many(:likes) }
-  #   it { should have_many(:requested_friendships) }
-  #   it { should have_many(:received_friendships) }
-  #   it { should have_many(:confirmed_friendships) }
-  # end
+  describe 'validations' do
+    it { should validate_presence_of(:username) }
+    it { should validate_presence_of(:fullname) }
+    it { should validate_presence_of(:email) }
+  end
 
-  # describe '#friends' do
-  #   friends_array = nil
-  #   it 'show something if there is a friendships' do
-  #     expect(friends_array).to be_nil
-  #   end
-  # end
+  describe 'associations' do
+    it { should have_many(:messages) }
+    it { should have_many(:active_relationships) }
+    it { should have_many(:passive_relationships) }
+    it { should have_many(:following) }
+    it { should have_many(:followers) }
+  end
 end
