a9ef43f4cd8a98ed59cb21cde119002dfa89c681
AlejandroToledo <atoledofr@gmail.com>
2021-01-13 11:26:29 -0500

    order the users by most recently created

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     @message = current_user.messages.build if logged_in
     @feed_items = current_user.feed.paginate(page: params[:page], per_page: 10) if logged_in
-    @users = User.all
+    @users = User.all.order(created_at: :desc)
   end
 
   private
