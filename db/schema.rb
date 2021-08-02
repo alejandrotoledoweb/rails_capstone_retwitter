07e17fa05a4238c5297ca871315c78dbd3cc1df5
Alejandro Toledo <atoledofr@gmail.com>
2021-08-02 16:46:05 -0500

    fix home view of app and center elements

--- db/schema.rb
+++ db/schema.rb
@@ -10,39 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_109_000_403) do
+ActiveRecord::Schema.define(version: 2021_01_09_000403) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'messages', force: :cascade do |t|
-    t.bigint 'user_id', null: false
-    t.text 'body'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index %w[user_id created_at], name: 'index_messages_on_user_id_and_created_at'
-    t.index ['user_id'], name: 'index_messages_on_user_id'
+  create_table "messages", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id", "created_at"], name: "index_messages_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
-  create_table 'relationships', force: :cascade do |t|
-    t.integer 'follower_id'
-    t.integer 'followed_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['followed_id'], name: 'index_relationships_on_followed_id'
-    t.index %w[follower_id followed_id], name: 'index_relationships_on_follower_id_and_followed_id', unique: true
-    t.index ['follower_id'], name: 'index_relationships_on_follower_id'
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'fullname'
-    t.string 'username'
-    t.string 'email'
-    t.string 'photo'
-    t.string 'cover_image'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'password_digest'
+  create_table "users", force: :cascade do |t|
+    t.string "fullname"
+    t.string "username"
+    t.string "email"
+    t.string "photo"
+    t.string "cover_image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
-  add_foreign_key 'messages', 'users'
+  add_foreign_key "messages", "users"
 end
